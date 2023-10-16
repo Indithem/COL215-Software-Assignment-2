5607645e48d876d1ae25ca118271feac9c330b8d
Sai Srinivas <128889127+Indithem@users.noreply.github.com>
2023-10-16 16:19:33 +0530

    made py files

---
+++ class_defs.py
@@ -0,0 +1,17 @@
+class Gate:
+
+    def __init__(self,type,delay,area,inps:arr,out):
+    # other than inps, all others are strings
+        pass
+
+    def update_area_and_delay(self, new_area, new_delay):
+        pass
+
+    def update_connection(new_inps, new_out):
+        pass
+
+    #make other functions as needed
+
+class Graphs:
+
+    #data structure to store a list of edges and vertices
