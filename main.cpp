fd82dc0bb85386a85528fd9c8f597f34e2d835f2
Indithem <128889127+Indithem@users.noreply.github.com>
2023-10-19 18:42:54 +0530

    bug fix inps

--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@
 
 int main(){
     Gate_Variants* variants = read_gateVarient_from("gate_delays.txt");
-    Circuit* test= read_circuit_from("circuit.txt");
-    test->print();
+    Circuit* test= read_circuit_from("circuit_with_loop.txt");
+    // test->print();
     set_Gates_to_min(test,variants);
     cout<<Finding_max_delay_in_a_circuit(test);
 }
