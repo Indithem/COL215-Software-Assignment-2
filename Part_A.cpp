d63a95c8b69e09862c44d99eb8a5d4a7bb2af50a
Hanumath2005 <cs1221608@iitd.ac.in>
2023-10-19 18:24:00 +0530

    changes

--- Part_A.cpp
+++ Part_A.cpp
@@ -4,10 +4,14 @@
 using namespace std;
 double Finding_max_delay_in_a_circuit(Circuit*given_circuit){
     unordered_map<string,Signal*>&out_puts_of_circuit=given_circuit->primary_outs;
+    unordered_map<string,Signal*>&inputs_of_circuit=given_circuit->primary_inps;
     unordered_map<Signal*,long double>result_outvalues;
     result_outvalues[nullptr]=0;
     vector<Signal*>&dff_in_the_circuit=given_circuit->DFFInps;
     stack<Signal*>temporary_storage;
+    for(auto i=inputs_of_circuit.begin();i!=inputs_of_circuit.end();i++){
+        result_outvalues[i->second]=0;
+    }
     for(int i=0;i<dff_in_the_circuit.size();i++){
         temporary_storage.push(dff_in_the_circuit[i]);
     }
@@ -18,19 +22,25 @@ double Finding_max_delay_in_a_circuit(Circuit*given_circuit){
     double final_answer=0;
     while(temporary_storage.empty()!=true){
         temp_store=temporary_storage.top();
-        if((result_outvalues.find(temp_store->get_source()->input1)!=result_outvalues.end())&&(result_outvalues.find(temp_store->get_source()->input2)!=result_outvalues.end())){
-            result_outvalues[temp_store]=max(result_outvalues[temp_store->get_source()->input1],result_outvalues[temp_store->get_source()->input2])+temp_store->get_source()->time;
-            if(final_answer<result_outvalues[temp_store]){
-                final_answer=result_outvalues[temp_store];
-            }
+        if(temp_store->get_source()->get_type()==dffGATE){
+            result_outvalues[temp_store]=0;
             temporary_storage.pop();
-            continue;
         }
-        if(result_outvalues.find(temp_store->get_source()->input1)==result_outvalues.end()){
-            temporary_storage.push(temp_store->get_source()->input1);            
-        }
-        if(result_outvalues.find(temp_store->get_source()->input2)==result_outvalues.end()){
-            temporary_storage.push(temp_store->get_source()->input2);
+        else{
+            if((result_outvalues.find(temp_store->get_source()->input1)!=result_outvalues.end())&&(result_outvalues.find(temp_store->get_source()->input2)!=result_outvalues.end())){
+                result_outvalues[temp_store]=max(result_outvalues[temp_store->get_source()->input1],result_outvalues[temp_store->get_source()->input2])+temp_store->get_source()->time;
+                if(final_answer<result_outvalues[temp_store]){
+                    final_answer=result_outvalues[temp_store];
+                }
+                temporary_storage.pop();
+                continue;
+            }
+            if(result_outvalues.find(temp_store->get_source()->input1)==result_outvalues.end()){
+                temporary_storage.push(temp_store->get_source()->input1);            
+            }
+            if(result_outvalues.find(temp_store->get_source()->input2)==result_outvalues.end()){
+                temporary_storage.push(temp_store->get_source()->input2);
+            }
         }
     }
     return final_answer;
---
+++ Part_B_helper.cpp
@@ -0,0 +1,10 @@
+/* Helpers
+inputs:i(base3),Ciruit,gate_variants,Area
+Outputs:Struct{next i,Area}*/
+#include<bits/stdc++.h>
+using namespace std;
+struct helper{
+    string next_iter;
+    long double iter_area;
+};
+string 
